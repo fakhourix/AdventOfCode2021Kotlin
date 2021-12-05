21959c1f0971d153f8fa521fac569dcc9877f84e
fakhouri <hani.fakhouri@verisure.com>
2021-12-05 11:25:52 +0100

    Day 4 solution

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,14 @@
 import day1.Puzzle1
 import day2.Puzzle2
 import day3.Puzzle3
+import day4.Puzzle4
 
 fun main() {
     listOf(
         Puzzle1(),
         Puzzle2(),
         Puzzle3(),
+        Puzzle4(),
     ).forEach {
         it.runPuzzle()
     }
